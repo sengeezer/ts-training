93954e7f45b59839eae3d870ce42bdec9b8574d6
Fabio Albertin <fabio.albertin@matchesfashion.com>
2018-02-16 16:09:42 +0000

    Finished greeter tutorial

--- greeter.js
+++ greeter.js
@@ -1,5 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello " + person;
+    return "Hello " + person.firstName + " " + person.lastName;
 }
-var user = 'Jane Doh';
+var user = new Student('Jahne', 'M', 'D\'Oh');
 document.body.innerHTML = greeter(user);
